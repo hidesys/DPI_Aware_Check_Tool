63f0712df08f4ee81f1c74ad1f083ba5d73855cf
hidesys <info@hidesys.net>
2014-04-04 17:03:35 +0900

    TransformToDevice Unshown Bug Fix

--- DPI_Aware_Check_Tool.False45/MainWindow.xaml.cs
+++ DPI_Aware_Check_Tool.False45/MainWindow.xaml.cs
@@ -22,34 +22,48 @@ namespace DPI_Aware_Check_Tool
         public MainWindow()
         {
             InitializeComponent();
-            this.DataContext = new MainWindowModel(PresentationSource.FromVisual(this));
-
         }
 
         private void TransformFromDevice_Reload_Click(object sender, RoutedEventArgs e)
         {
-            this.DataContext = new MainWindowModel(PresentationSource.FromVisual(this));
+            this.DataContext = new MainWindowModel(PresentationSource.FromVisual(this).CompositionTarget.TransformFromDevice);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.DataContext = new MainWindowModel(PresentationSource.FromVisual(this).CompositionTarget.TransformFromDevice);
         }
     }
 
-    public partial class MainWindowModel
+    public partial class MainWindowModel : System.ComponentModel.INotifyPropertyChanged 
     {
-        public MainWindowModel(PresentationSource presentationSource)
+        public MainWindowModel(Matrix transformFromDevice)
         {
-            if (presentationSource != null)
-            {
-                _TransformFromDevice = presentationSource.CompositionTarget.TransformFromDevice;
-            }
+            TransformFromDevice = transformFromDevice;
         }
 
+        public const string TransformFromDevicePropertyName = "TransformFromDevice";
         private Matrix _TransformFromDevice;
         public Matrix TransformFromDevice
-        { get { return _TransformFromDevice; } }
+        {
+            get { return _TransformFromDevice; }
+            set { if (value != _TransformFromDevice || true) { _TransformFromDevice = value; OnPropertyChanged(TransformFromDevicePropertyName); } }
+        }
 
         public Version Version
         { get { return Environment.OSVersion.Version; } }
 
         public string Title
         { get { return "DPI Aware Check Tool, " + TitleAdditional; } }
+
+        public event System.ComponentModel.PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new System.ComponentModel.PropertyChangedEventArgs(name));
+            }
+        }
     }
 }
